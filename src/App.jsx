ec63a2fac2471439fe62d5b85e51b088931e1024
Voices of Change <sustainthevoices@gmail.com>
2026-04-03 00:31:47 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import PageNotFound from './lib/PageNotFound';
 import Layout from './components/Layout';
 import Home from './pages/Home';
